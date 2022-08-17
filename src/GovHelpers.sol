7de52196667e7f411d3c5ba403138948451a1dee
Lukas <lukasstrassel@googlemail.com>
2022-08-17 12:11:14 +0200

    feat: add missing governance methods (#10)
    * feat: add missing governance methods
    
    * fix: add missing structs

--- src/GovHelpers.sol
+++ src/GovHelpers.sol
@@ -2,9 +2,46 @@
 pragma solidity >=0.7.5 <0.9.0;
 pragma abicoder v2;
 
-import "forge-std/Vm.sol";
+import 'forge-std/Vm.sol';
 
 interface IAaveGov {
+  enum ProposalState {
+    Pending,
+    Canceled,
+    Active,
+    Failed,
+    Succeeded,
+    Queued,
+    Expired,
+    Executed
+  }
+
+  struct Vote {
+    bool support;
+    uint248 votingPower;
+  }
+
+  struct Proposal {
+    uint256 id;
+    address creator;
+    address executor;
+    address[] targets;
+    uint256[] values;
+    string[] signatures;
+    bytes[] calldatas;
+    bool[] withDelegatecalls;
+    uint256 startBlock;
+    uint256 endBlock;
+    uint256 executionTime;
+    uint256 forVotes;
+    uint256 againstVotes;
+    bool executed;
+    bool canceled;
+    address strategy;
+    bytes32 ipfsHash;
+    mapping(address => Vote) votes;
+  }
+
   struct ProposalWithoutVotes {
     uint256 id;
     address creator;
@@ -25,17 +62,6 @@ interface IAaveGov {
     bytes32 ipfsHash;
   }
 
-  enum ProposalState {
-    Pending,
-    Canceled,
-    Active,
-    Failed,
-    Succeeded,
-    Queued,
-    Expired,
-    Executed
-  }
-
   struct SPropCreateParams {
     address executor;
     address[] targets;
@@ -46,6 +72,16 @@ interface IAaveGov {
     bytes32 ipfsHash;
   }
 
+  /**
+   * @dev Creates a Proposal (needs Proposition Power of creator > Threshold)
+   * @param executor The ExecutorWithTimelock contract that will execute the proposal
+   * @param targets list of contracts called by proposal's associated transactions
+   * @param values list of value in wei for each propoposal's associated transaction
+   * @param signatures list of function signatures (can be empty) to be used when created the callData
+   * @param calldatas list of calldatas: if associated signature empty, calldata ready, else calldata is arguments
+   * @param withDelegatecalls if true, transaction delegatecalls the taget, else calls the target
+   * @param ipfsHash IPFS hash of the proposal
+   **/
   function create(
     address executor,
     address[] memory targets,
@@ -56,37 +92,146 @@ interface IAaveGov {
     bytes32 ipfsHash
   ) external returns (uint256);
 
+  /**
+   * @dev Cancels a Proposal,
+   * either at anytime by guardian
+   * or when proposal is Pending/Active and threshold no longer reached
+   * @param proposalId id of the proposal
+   **/
+  function cancel(uint256 proposalId) external;
+
+  /**
+   * @dev Queue the proposal (If Proposal Succeeded)
+   * @param proposalId id of the proposal to queue
+   **/
   function queue(uint256 proposalId) external;
 
+  /**
+   * @dev Execute the proposal (If Proposal Queued)
+   * @param proposalId id of the proposal to execute
+   **/
   function execute(uint256 proposalId) external payable;
 
+  /**
+   * @dev Function allowing msg.sender to vote for/against a proposal
+   * @param proposalId id of the proposal
+   * @param support boolean, true = vote for, false = vote against
+   **/
   function submitVote(uint256 proposalId, bool support) external;
 
-  function getProposalById(uint256 proposalId)
-    external
-    view
-    returns (ProposalWithoutVotes memory);
+  /**
+   * @dev Function to register the vote of user that has voted offchain via signature
+   * @param proposalId id of the proposal
+   * @param support boolean, true = vote for, false = vote against
+   * @param v v part of the voter signature
+   * @param r r part of the voter signature
+   * @param s s part of the voter signature
+   **/
+  function submitVoteBySignature(
+    uint256 proposalId,
+    bool support,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external;
 
-  function getProposalState(uint256 proposalId)
-    external
-    view
-    returns (ProposalState);
+  /**
+   * @dev Set new GovernanceStrategy
+   * Note: owner should be a timelocked executor, so needs to make a proposal
+   * @param governanceStrategy new Address of the GovernanceStrategy contract
+   **/
+  function setGovernanceStrategy(address governanceStrategy) external;
+
+  /**
+   * @dev Set new Voting Delay (delay before a newly created proposal can be voted on)
+   * Note: owner should be a timelocked executor, so needs to make a proposal
+   * @param votingDelay new voting delay in seconds
+   **/
+  function setVotingDelay(uint256 votingDelay) external;
+
+  /**
+   * @dev Add new addresses to the list of authorized executors
+   * @param executors list of new addresses to be authorized executors
+   **/
+  function authorizeExecutors(address[] memory executors) external;
+
+  /**
+   * @dev Remove addresses to the list of authorized executors
+   * @param executors list of addresses to be removed as authorized executors
+   **/
+  function unauthorizeExecutors(address[] memory executors) external;
+
+  /**
+   * @dev Let the guardian abdicate from its priviledged rights
+   **/
+  function __abdicate() external;
+
+  /**
+   * @dev Getter of the current GovernanceStrategy address
+   * @return The address of the current GovernanceStrategy contracts
+   **/
+  function getGovernanceStrategy() external view returns (address);
+
+  /**
+   * @dev Getter of the current Voting Delay (delay before a created proposal can be voted on)
+   * Different from the voting duration
+   * @return The voting delay in seconds
+   **/
+  function getVotingDelay() external view returns (uint256);
+
+  /**
+   * @dev Returns whether an address is an authorized executor
+   * @param executor address to evaluate as authorized executor
+   * @return true if authorized
+   **/
+  function isExecutorAuthorized(address executor) external view returns (bool);
+
+  /**
+   * @dev Getter the address of the guardian, that can mainly cancel proposals
+   * @return The address of the guardian
+   **/
+  function getGuardian() external view returns (address);
+
+  /**
+   * @dev Getter of the proposal count (the current number of proposals ever created)
+   * @return the proposal count
+   **/
+  function getProposalsCount() external view returns (uint256);
+
+  /**
+   * @dev Getter of a proposal by id
+   * @param proposalId id of the proposal to get
+   * @return the proposal as ProposalWithoutVotes memory object
+   **/
+  function getProposalById(uint256 proposalId) external view returns (ProposalWithoutVotes memory);
+
+  /**
+   * @dev Getter of the Vote of a voter about a proposal
+   * Note: Vote is a struct: ({bool support, uint248 votingPower})
+   * @param proposalId id of the proposal
+   * @param voter address of the voter
+   * @return The associated Vote memory object
+   **/
+  function getVoteOnProposal(uint256 proposalId, address voter) external view returns (Vote memory);
+
+  /**
+   * @dev Get the current state of a proposal
+   * @param proposalId id of the proposal
+   * @return The current state if the proposal
+   **/
+  function getProposalState(uint256 proposalId) external view returns (ProposalState);
 }
 
 library GovHelpers {
-  IAaveGov internal constant GOV =
-    IAaveGov(0xEC568fffba86c094cf06b22134B23074DFE2252c);
+  IAaveGov internal constant GOV = IAaveGov(0xEC568fffba86c094cf06b22134B23074DFE2252c);
 
-  address public constant SHORT_EXECUTOR =
-    0xEE56e2B3D491590B5b31738cC34d5232F378a8D5;
+  address public constant SHORT_EXECUTOR = 0xEE56e2B3D491590B5b31738cC34d5232F378a8D5;
 
-  address public constant LONG_EXECUTOR =
-    0x61910EcD7e8e942136CE7Fe7943f956cea1CC2f7;
+  address public constant LONG_EXECUTOR = 0x61910EcD7e8e942136CE7Fe7943f956cea1CC2f7;
 
   address public constant AAVE = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
 
-  address internal constant AAVE_WHALE =
-    address(0x25F2226B597E8F9514B3F68F00f494cF4f286491);
+  address internal constant AAVE_WHALE = address(0x25F2226B597E8F9514B3F68F00f494cF4f286491);
 
   /**
    * Impersonate the ecosystem reserve and created the proposal.
@@ -112,10 +257,7 @@ library GovHelpers {
 
   function _getProposalSlot(uint256 proposalId) private pure returns (bytes32 slot) {
     uint256 proposalsMapSlot = 0x4;
-    return
-      bytes32(
-        uint256(keccak256(abi.encode(proposalId, proposalsMapSlot))) + 11
-      );
+    return bytes32(uint256(keccak256(abi.encode(proposalId, proposalsMapSlot))) + 11);
   }
 
   /**
