5481ac5ca51c76f87dec0ef316942c51f10515dc
Lukas <lukasstrassel@googlemail.com>
2023-12-25 21:36:57 +0100

    fix: patch usdc (#203)
    * fix: patch usdc
    
    * fix: revert change
    
    * fix: updc patches

--- src/CommonTestBase.sol
+++ src/CommonTestBase.sol
@@ -8,6 +8,9 @@ import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
 import {AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
 import {AaveV3OptimismAssets} from 'aave-address-book/AaveV3Optimism.sol';
 import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
+import {AaveV3AvalancheAssets} from 'aave-address-book/AaveV3Avalanche.sol';
+import {AaveV3BaseAssets} from 'aave-address-book/AaveV3Base.sol';
 import {ChainIds} from './ChainIds.sol';
 import {GovV3Helpers} from './GovV3Helpers.sol';
 
@@ -80,11 +83,21 @@ contract CommonTestBase is Test {
         IERC20(asset).transfer(user, amount);
         return true;
       }
+      if (asset == AaveV3EthereumAssets.USDC_UNDERLYING) {
+        vm.startPrank(0xcEe284F754E854890e311e3280b767F80797180d);
+        IERC20(asset).transfer(user, amount);
+        return true;
+      }
     }
     if (block.chainid == ChainIds.OPTIMISM) {
       // sUSD
       if (asset == AaveV3OptimismAssets.sUSD_UNDERLYING) {
         vm.startPrank(AaveV3OptimismAssets.sUSD_A_TOKEN);
+        IERC20(asset).transfer(user, amount);
+        return true;
+      }
+      if (asset == AaveV3OptimismAssets.USDCn_UNDERLYING) {
+        vm.startPrank(0xf491d040110384DBcf7F241fFE2A546513fD873d);
         IERC20(asset).transfer(user, amount);
         return true;
       }
@@ -94,6 +107,27 @@ contract CommonTestBase is Test {
       // EURs
       if (asset == 0xcB444e90D8198415266c6a2724b7900fb12FC56E) {
         vm.startPrank(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+        IERC20(asset).transfer(user, amount);
+        return true;
+      }
+    }
+    if (block.chainid == ChainIds.POLYGON) {
+      if (asset == AaveV3PolygonAssets.USDCn_UNDERLYING) {
+        vm.startPrank(0xe7804c37c13166fF0b37F5aE0BB07A3aEbb6e245);
+        IERC20(asset).transfer(user, amount);
+        return true;
+      }
+    }
+    if (block.chainid == ChainIds.AVALANCHE) {
+      if (asset == AaveV3AvalancheAssets.USDC_UNDERLYING) {
+        vm.startPrank(0x9f8c163cBA728e99993ABe7495F06c0A3c8Ac8b9);
+        IERC20(asset).transfer(user, amount);
+        return true;
+      }
+    }
+    if (block.chainid == ChainIds.BASE) {
+      if (asset == AaveV3BaseAssets.USDC_UNDERLYING) {
+        vm.startPrank(0x20FE51A9229EEf2cF8Ad9E89d91CAb9312cF3b7A);
         IERC20(asset).transfer(user, amount);
         return true;
       }
--- tests/ProtocolV3TestBase.t.sol
+++ tests/ProtocolV3TestBase.t.sol
@@ -6,6 +6,7 @@ import {ProtocolV3LegacyTestBase, ProtocolV3TestBase, ReserveConfig} from '../sr
 import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 import {AaveV3Polygon, AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
 import {AaveV3Optimism, AaveV3OptimismAssets} from 'aave-address-book/AaveV3Optimism.sol';
+import {AaveV3Avalanche, AaveV3AvalancheAssets} from 'aave-address-book/AaveV3Avalanche.sol';
 import {PayloadWithEmit} from './mocks/PayloadWithEmit.sol';
 
 contract ProtocolV3TestBaseTest is ProtocolV3TestBase {
@@ -65,6 +66,20 @@ contract ProtocolV3TestE2ETestAll is ProtocolV3TestBase {
 
   function test_e2e() public {
     e2eTest(AaveV3Optimism.POOL);
+  }
+}
+
+contract ProtocolV3TestE2ETestAvalancheAll is ProtocolV3TestBase {
+  function setUp() public {
+    vm.createSelectFork('avalanche', 38700698);
+  }
+
+  function test_e2e() public {
+    e2eTest(AaveV3Avalanche.POOL);
+  }
+
+  function test_deal() public {
+    deal2(AaveV3AvalancheAssets.USDC_UNDERLYING, address(this), 1000);
   }
 }
 
