ffdfec57ed1b7f90afbcdb108efc1141e91bc479
Lukas <lukasstrassel@googlemail.com>
2024-03-24 22:29:02 +0100

    fix: bnb naming (#282)

--- src/v3-config-engine/AaveV3PayloadBNB.sol
+++ src/v3-config-engine/AaveV3PayloadBNB.sol
@@ -8,7 +8,7 @@ import './AaveV3Payload.sol';
  * @dev Base smart contract for an Aave v3.0.2 (compatible with 3.0.0) listing on v3 Bnb.
  * @author BGD Labs
  */
-abstract contract AaveV3PayloadBnb is AaveV3Payload(IEngine(AaveV3BNB.CONFIG_ENGINE)) {
+abstract contract AaveV3PayloadBNB is AaveV3Payload(IEngine(AaveV3BNB.CONFIG_ENGINE)) {
   function getPoolContext() public pure override returns (IEngine.PoolContext memory) {
     return IEngine.PoolContext({networkName: 'BNB Smart Chain', networkAbbreviation: 'Bnb'});
   }
