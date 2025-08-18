a4f8a4efe598cb8b84aaa768829575916c8a0fc5
Harsh Pandey <harshsatishpandey@gmail.com>
2025-08-18 11:54:11 +0530

    feat: whitelabel (#469)
    * feat: gov helpers for ppc
    
    * fix: libs
    
    * fix: defaultTest for custom pc
    
    * refactor: defaultTest
    
    * fix: createPermissionedPayloadCalldata
    
    * feat: add ink
    
    * chore: add engine payload
    
    * feat: seatbelt utils
    
    * fix: seatbelt
    
    * chore: update submodule
    
    * fix: add flag for running seatbelt
    
    * chore: bump cli version
    
    * chore: add whitelabel to name
    
    * chore: fix lint
    
    * fix: more graceful ffi
    
    * fix: remove duplicate code and more docs
    
    * chore: fix lint

--- src/CommonTestBase.sol
+++ src/CommonTestBase.sol
@@ -15,6 +15,7 @@ import {AaveV3ArbitrumAssets} from 'aave-address-book/AaveV3Arbitrum.sol';
 import {AaveV3GnosisAssets} from 'aave-address-book/AaveV3Gnosis.sol';
 import {AaveV3BaseAssets} from 'aave-address-book/AaveV3Base.sol';
 import {ChainIds} from 'solidity-utils/contracts/utils/ChainHelpers.sol';
+import {IPool} from 'aave-address-book/AaveV3.sol';
 import {GovV3Helpers} from './GovV3Helpers.sol';
 
 struct ReserveTokens {
@@ -32,6 +33,14 @@ contract CommonTestBase is Test {
 
   function executePayload(Vm vm, address payload) internal {
     GovV3Helpers.executePayload(vm, payload);
+  }
+
+  function executePayload(Vm vm, address payload, IPool pool) internal {
+    GovV3Helpers.executePayload(
+      vm,
+      payload,
+      address(GovV3Helpers.getPayloadsController(pool, block.chainid))
+    );
   }
 
   /**
